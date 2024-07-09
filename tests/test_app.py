5844b7f577100becf17a7f2664d74ac33e0806b9
eduardopanssa <eduardopanissa@gmail.com>
2024-07-09 20:52:12 -0300

    reajuste

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-from curso_fast_zero.app import app
+from fast_zero.app import app
 
 
 def test_read_root_deve_retornar_ok_e_ola_mundo():
